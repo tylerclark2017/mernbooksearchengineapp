7b5343011c30bc0858cd016220d9cdbbd6e25e1c
Tyler Clark <tylerc111@gmail.com>
2024-04-19 11:32:32 -0400

    made improvements to the typeDefs.js file

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,21 +1,37 @@
 const typeDefs = `
 type Book {
-    authors: String,
-    description: String,
-    bookId: String,
-    image: String,
-    link: String,
-    title: String,
+    authors: [String]
+    description: String
+    bookId: ID
+    image: String
+    link: String
+    title: String
 },
 type User {
-username: String,
-email: String,
-savedBooks: [Book],
-bookCount: Int,
+username: String
+email: String
+savedBooks: [Book]
+bookCount: Int
 },
 type Query {
-    books: [Book],
-    book(bookId: ID!): Book,
-    users: [User],
-    user(username: String!): User,
-}`
+    books: [Book]
+    book(bookId: ID!): Book
+    users: [User]
+    user(username: String!): User
+},
+type Mutation {
+    addUser(username: String!, email: String!): User
+    saveBook(bookData: BookInput!): User
+    removeBook(bookId: ID!): User
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String
+    bookId: ID
+    image: String
+    link: String
+    title: String
+  }`
+
+module.exports = typeDefs;
