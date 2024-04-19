dc9a0801c281109a21608786e2ff57e66370b6e6
Tyler Clark <tylerc111@gmail.com>
2024-04-19 11:16:47 -0400

    first attempt at correctly setting up typeDefs.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,22 @@
+const typeDefs = `
+type Book {
+    authors: String,
+    description: String,
+    bookId: String,
+    image: String,
+    link: String,
+    title: String,
+},
+type User {
+username: String,
+email: String,
+password: String,
+savedBooks: [Book],
+bookCount: Int,
+},
+type Query {
+    books: [Book],
+    book(bookId: ID!): Book,
+    users: [User],
+    user(username: String!): User,
+}`
