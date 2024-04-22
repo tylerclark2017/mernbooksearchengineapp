de6ae2d4c9cc1f95cfedfc591e6576e77db43ffc
Tyler Clark <tylerc111@gmail.com>
2024-04-22 10:48:01 -0400

    added additional variables to server.js to ensure server functions as expected

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
-
+const { authMiddleware } = require('./utils/auth');
 const { ApolloServer } = require('@apollo/server');
 const { expressMiddleware } = require('@apollo/server/express4');
 const { typeDefs, resolvers } = require('./schemas');
